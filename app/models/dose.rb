784cc02be612922742051232ec6bbf7b5dbc4dde
Alexander Stones <amstones@hotmail.co.uk>
2019-07-30 19:14:50 +0100

    made root

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+
+  validates :description, presence: true
+
+  belongs_to :ingredient
+  belongs_to :cocktail
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+json = open(url).read
+
+drinks = JSON.parse(json)
+# p drinks['drinks'].class
+drinks["drinks"].each do |array|
+  Ingredient.create(name: array["strIngredient1"])
+end
+
+10.times do
+  Cocktail.create(name: "Booze#{rand(1..100)}")
+end
